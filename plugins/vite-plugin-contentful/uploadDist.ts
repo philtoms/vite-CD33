227d6d9c665372041ec7458d0793bdbe18033ac0
Phil Toms <phil.toms@hotmail.co.uk>
2021-05-25 20:06:17 +0100

    feat: save client manifest

--- plugins/vite-plugin-contentful/uploadDist.ts
+++ plugins/vite-plugin-contentful/uploadDist.ts
@@ -6,6 +6,8 @@ import fs from 'fs'
 import crc from 'node-crc'
 import dotenv from 'dotenv'
 
+const base = process.cwd()
+
 dotenv.config()
 
 type Version = Record<
@@ -51,7 +53,7 @@ const uploadFile = (Body: string, Key: string, versionPath: string) => {
   const command = new PutObjectCommand(uploadParams)
   return new Promise((resolve) => {
     client.send(command).then(() => {
-      resolve(`uploaded ${Key} to S3:${Bucket}`)
+      resolve(`uploaded ${Key} to S3:${Bucket}: ${versionPath}`)
     })
   })
 }
@@ -69,30 +71,13 @@ const versionKey = (root: string, isServer: boolean, key: string, source: string
     : path.join(isServer ? 'server' : 'client', key)
 }
 
-const createVersionManifest = (isServer: boolean, root: string): Version => {
-  let version = {}
-  if (isServer) {
-    const fileName = 'manifest.json'
-    const moduleSource = fs.readFileSync(path.join(__dirname, '../../dist/client', fileName), 'utf-8')
-    const specifier = versionKey(root, false, fileName, moduleSource)
-    const file = versionKey('', false, fileName, moduleSource)
-    version = {
-      'client/manifest': {
-        specifier,
-        file
-      }
-    }
-  }
-  return version
-}
-
 async function uploadDist(dir: string, bundle: OutputBundle) {
   const isServer = dir.endsWith('/server')
   const status = await git.status()
   const { current } = status
   const root = current || ''
 
-  const version = createVersionManifest(isServer, root)
+  const version: Version = {}
 
   const results = await Promise.all(
     Object.entries(bundle).map(([name, item]) => {
@@ -109,12 +94,12 @@ async function uploadDist(dir: string, bundle: OutputBundle) {
       return uploadFile(moduleSource, specifier, versionPath)
     })
   )
-  if (isServer) {
-    console.log('\nVersion manifest', version)
-    const manifestKey = path.join(root, 'manifest.json')
-    const log = await uploadFile(JSON.stringify(version), manifestKey, manifestKey)
-    results.push(log)
-  }
+
+  console.log('\nVersion manifest', version)
+  const versionSource = JSON.stringify(version)
+  const manifestKey = path.join(root, isServer ? 'server' : 'client', 'manifest.json')
+  const log = await uploadFile(versionSource, manifestKey, manifestKey)
+  results.push(log)
   for (const result of results) {
     console.log(result)
   }

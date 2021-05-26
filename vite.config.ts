ff516d1766d9bbea5e6df5873dce4634074cad52
Phil Toms <phil.toms@hotmail.co.uk>
2021-05-26 15:37:25 +0100

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,29 @@ import { UserConfig } from 'vite'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import contentful from './plugins/vite-plugin-contentful'
+import typescript from '@rollup/plugin-typescript'
+
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default
+
+// 2. create a transformer;
+// the factory additionally accepts an options object which described below
+const styledComponentsTransformer = createStyledComponentsTransformer({ ssr: false })
+
+const styledComponentsSSR = typescript({
+  transformers: [
+    () => ({
+      before: [styledComponentsTransformer]
+    })
+  ]
+})
 
 const isBuild = !!process.env.BUILD
 
 const config: UserConfig = {
   // plugins: isBuild ? [ssr(), { ...contentful(), enforce: 'pre' }, nodeResolve(), commonjs()] : [reactRefresh(), ssr()]
-  plugins: isBuild ? [ssr(), { ...contentful(), enforce: 'pre' }, nodeResolve()] : [reactRefresh(), ssr()]
+  plugins: isBuild
+    ? [ssr(), { ...contentful(), enforce: 'pre' }, nodeResolve(), styledComponentsSSR]
+    : [reactRefresh(), ssr(), styledComponentsSSR]
 }
 
 export default config

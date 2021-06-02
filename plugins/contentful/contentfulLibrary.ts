7d51e26735e37e5d9986f5e0faa35411c14f2ade
Phil Toms <phil.toms@hotmail.co.uk>
2021-06-02 13:05:49 +0100

    feat: live contentful data

---
+++ plugins/contentful/contentfulLibrary.ts
@@ -0,0 +1,33 @@
+import { createClient } from 'contentful'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PREVIEW_ENV = process.env.CONTENTFUL_ENVIRONMENT || 'master'
+const PREVIEW_SPACE = process.env.CONTENTFUL_SPACE_ID || ''
+const PREVIEW_ACCESS_TOKEN = process.env.PREVIEW_ACCESS_TOKEN || ''
+
+const CDN_ENV = process.env.CONTENTFUL_ENVIRONMENT || 'master'
+const CDN_SPACE = process.env.CONTENTFUL_SPACE_ID || ''
+const CDN_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN || ''
+
+export const getContentFromContentful = (entryId: string, isPublish: boolean = false) => {
+  const environment = isPublish ? CDN_ENV : PREVIEW_ENV
+  const space = isPublish ? CDN_SPACE : PREVIEW_SPACE
+  const accessToken = isPublish ? CDN_ACCESS_TOKEN : PREVIEW_ACCESS_TOKEN
+  const host = isPublish ? 'cdn.contentful.com' : 'preview.contentful.com'
+
+  console.log({ host, environment, space, accessToken: '****' })
+
+  const client = createClient({
+    environment,
+    space,
+    accessToken,
+    host
+  })
+
+  return client.getEntry(entryId, { include: 2 }).catch((e) => {
+    console.log(e)
+    return { fields: null }
+  })
+}
